865bef15c743ec2700e8cf64bf6453b739906807
Manickam10 <141651496+Manickam10@users.noreply.github.com>
2023-12-02 10:25:10 +0530

    Day4 board game updated with function

--- Daily exercise/Day4/board game.py
+++ Daily exercise/Day4/board game.py
@@ -23,37 +23,44 @@ def print_board(board):
 def roll_dice():
     return random.randint(1,6) # returns a random number between 1 and 6
 
+#Function to play the game
+def player_turn(player_name):
+    player_score=0
+    row_A = roll_dice()-1
+    col_A = roll_dice()-1
+    print(f"rolled:{row_A+1},{col_A+1}\n")
+    #if  player B already has their initial in the same row,col add a point to A and change the initial to A.
+    if board[row_A][col_A] != player_name and board[row_A][col_A] != '*': 
+        player_score = 1
+        board[row_A][col_A] = player_name
+        print_board(board)   
+    else:
+        board[row_A][col_A] = player_name
+        print_board(board) 
+    return player_score        
+
+
 if __name__ =="__main__":
     #Initializing or creating board
     board=[]
-    for i in range(6):
+    row = 6
+    col= 6 
+    for i in range(row):
         col_board=[]
-        for j in range(6):
+        for j in range(col):
             col_board.append('*')   
         board.append(col_board)
-    
+    print_board(board)
     player_A_score = 0
     player_B_score = 0
 
     while player_A_score < 5 and player_B_score < 5:
         
         #Player A's turn
-        user_input_a = input("Player A, Enter roll(r)...")
+        user_input_a = input("\nPlayer A, Enter roll(r)...")
         if user_input_a.lower() == 'r':
-            row_A = roll_dice()-1
-            col_A = roll_dice()-1
-            print(f"first roll:{row_A+1},second roll:{col_A+1}")
-            #if  player B already has their initial in the same row,col add a point to A and change the initial to A.
-            if board[row_A][col_A] == 'B': 
-                player_A_score += 1
-                board[row_A][col_A] = 'A'
-                print_board(board) 
-                print(f"Score:A-{player_A_score},Score:B-{player_B_score}")
-            else:
-                board[row_A][col_A] = 'A'
-                print_board(board) 
-                print(f"Score:A - {player_A_score},Score:B - {player_B_score}")
-
+            player_A_score += player_turn('A')
+            print(f"Score:A-{player_A_score},B-{player_B_score}")
             if player_A_score >= 5:
                 print("Player A wins!")
                 break
@@ -63,23 +70,10 @@ if __name__ =="__main__":
             
         
         #Player B's turn
-        user_input_b = input("Player B, Enter roll(r)...")
+        user_input_b = input("\nPlayer B, Enter roll(r)...")
         if user_input_b.lower() == 'r':
-            row_B = roll_dice() - 1
-            col_B = roll_dice() - 1
-            print(f"\nfirst roll:{row_B+1},second roll:{col_B+1}")
-            #if  player A already has their initial in the same row,col add a point to B and change the initial to B.
-            if board[row_B][col_B] == 'A':
-                player_B_score += 1
-                board[row_B][col_B] = 'B'
-                print_board(board)
-                print(f"Score: A - {player_A_score}, B - {player_B_score}")
-            else:
-                board[row_B][col_B] = 'B'
-                print_board(board)
-                print(f"Score: A - {player_A_score}, B - {player_B_score}")
-
-
+            player_B_score += player_turn('B')
+            print(f"Score: A - {player_A_score}, B - {player_B_score}")
             if player_B_score >= 5:
                 print("Player B wins!")
                 break
@@ -88,46 +82,48 @@ if __name__ =="__main__":
             break
 
 #output
+# * * * * * *
+# * * * * * *
+# * * * * * *
+# * * * * * *
+# * * * * * *
+# * * * * * *
+
+
 # Player A, Enter roll(r)...r
-# first roll:3,second roll:1
+# rolled:1,5
+
+# * * * * A *
 # * * * * * *
 # * * * * * *
-# A * * * * *
 # * * * * * *
 # * * * * * *
 # * * * * * *
 
-# Score:A - 0,Score:B - 0   
+# Score:A-0,B-0
+
 # Player B, Enter roll(r)...r
+# rolled:4,1
 
-# first roll:4,second roll:5
+# * * * * A *
 # * * * * * *
 # * * * * * *
-# A * * * * *
-# * * * * B *
+# B * * * * *
 # * * * * * *
 # * * * * * *
 
 # Score: A - 0, B - 0
+
 # Player A, Enter roll(r)...r
-# first roll:1,second roll:4
-# * * * A * *
+# rolled:1,2
+
+# * A * * A *
 # * * * * * *
-# A * * * * *
-# * * * * B *
+# * * * * * *
+# B * * * * *
 # * * * * * *
 # * * * * * *
 
-# Score:A - 0,Score:B - 0
-# Player B, Enter roll(r)...r
+# Score:A-0,B-0
 
-# first roll:6,second roll:2
-# * * * A * *
-# * * * * * *
-# A * * * * *
-# * * * * B *
-# * * * * * *
-# * B * * * *
-
-# Score: A - 0, B - 0
-# Player A, Enter roll(r)...
+# Player B, Enter roll(r)...
